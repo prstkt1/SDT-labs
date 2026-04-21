1f4c95b7099ca6dce8d8da1dc9296b02d4384697
prstkt1 <oliinyk.nikita@lll.kpi.ua>
2026-04-21 19:25:43 +0300

    setup CI pipeline with eslint, jest, and github actions

---
+++ jest.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  preset: "ts-jest",
+  testEnvironment: "node",
+  collectCoverage: true,
+  coverageDirectory: "coverage",
+  coverageThreshold: {
+    global: {
+      branches: 40,
+      functions: 40,
+      lines: 40,
+      statements: 40,
+    },
+  },
+};
