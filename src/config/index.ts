7a30dbe3ba50de6858fda20327faa50dc3df600b
prstkt1 <oliinyk.nikita@lll.kpi.ua>
2026-04-21 19:53:37 +0300

    fix

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,12 +1,23 @@
 import fs from "fs";
 import localConfig from "../../config.json";
 
-let config: Record<string, unknown>;
+export interface AppConfig {
+  db: {
+    user?: string;
+    password?: string;
+    host?: string;
+    port?: number;
+    database?: string;
+  };
+  [key: string]: unknown;
+}
+
+let config: AppConfig;
 
 try {
   config = JSON.parse(fs.readFileSync("/etc/mywebapp/config.json", "utf-8"));
 } catch {
-  config = localConfig;
+  config = localConfig as AppConfig;
 }
 
 export default config;
